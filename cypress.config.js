61c26a642c76fcd33a8c2d037fbf4dde0cd506f8
Dionatas Muniz <dionatasmuniz@gmail.com>
2024-12-27 11:03:38 -0300

    Adding Report

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,13 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.amazon.com.br', 
     supportFile: false,
+    reporter: 'mochawesome', 
+    reporterOptions: {
+      reportDir: 'cypress/results', // Diretório onde os relatórios serão salvos
+      overwrite: false,             // Não sobrescrever os relatórios anteriores
+      html: false,                  
+      json: true                    // Gerar relatório JSON
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
